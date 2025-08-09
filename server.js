bf719a96f0ae5086977fcbba492506234276fa0e
ac.sxlly <discordbotdeveloper9000@gmail.com>
2025-08-09 05:31:06 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { Client, GatewayIntentBits } = require('discord.js');
+import express from 'express';
+import bodyParser from 'body-parser';
+import { Client, GatewayIntentBits } from 'discord.js';
 
 const app = express();
 app.use(bodyParser.json());
